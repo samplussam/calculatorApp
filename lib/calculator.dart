32f7ebaf8b96e7d63c90ad805a1dad2921d6924f
samplussam <samstevens2002@gmail.com>
2022-09-28 18:39:00 +0100

    Added simple logic and updated UI

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -15,7 +15,7 @@ class Calculator extends StatefulWidget {
 class _CalculatorState extends State<Calculator> {
   Widget numButton(String btnText, Color btnColour, Color txtColour) {
     return ElevatedButton(
-      onPressed: () => {},
+      onPressed: () => {calculate(btnText)},
       style: ElevatedButton.styleFrom(
         fixedSize: Size(70, 70),
         backgroundColor: btnColour,
@@ -43,66 +43,92 @@ class _CalculatorState extends State<Calculator> {
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
-              children: const [
+              children: [
                 Padding(
                   padding: EdgeInsets.all(10.0),
                   child: Text(
-                    "0",
+                    text,
                     textAlign: TextAlign.left,
                     style: TextStyle(color: Colors.white, fontSize: 80),
                   ),
                 ),
               ],
             ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              numButton("C", Colors.green, Colors.black),
-              numButton("+/-", Colors.green, Colors.black),
-              numButton("%", Colors.green, Colors.black),
-              numButton("/", Colors.lightGreen, Colors.black),
-            ]),
             SizedBox(height: 10),
             Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              numButton("7", Colors.greenAccent, Colors.black),
-              numButton("8", Colors.greenAccent, Colors.black),
-              numButton("9", Colors.greenAccent, Colors.black),
+              numButton("7", Colors.green, Colors.black),
+              numButton("8", Colors.green, Colors.black),
+              numButton("9", Colors.green, Colors.black),
               numButton("X", Colors.lightGreen, Colors.black),
             ]),
             SizedBox(height: 10),
             Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              numButton("4", Colors.greenAccent, Colors.black),
-              numButton("5", Colors.greenAccent, Colors.black),
-              numButton("6", Colors.greenAccent, Colors.black),
+              numButton("4", Colors.green, Colors.black),
+              numButton("5", Colors.green, Colors.black),
+              numButton("6", Colors.green, Colors.black),
               numButton("-", Colors.lightGreen, Colors.black),
             ]),
             SizedBox(height: 10),
             Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              numButton("1", Colors.greenAccent, Colors.black),
-              numButton("2", Colors.greenAccent, Colors.black),
-              numButton("3", Colors.greenAccent, Colors.black),
+              numButton("1", Colors.green, Colors.black),
+              numButton("2", Colors.green, Colors.black),
+              numButton("3", Colors.green, Colors.black),
               numButton("+", Colors.lightGreen, Colors.black),
             ]),
             SizedBox(height: 10),
             Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              ElevatedButton(
-                onPressed: (() => {}),
-                style: ElevatedButton.styleFrom(
-                  shape: StadiumBorder(),
-                  backgroundColor: (Colors.lightGreenAccent),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(55, 12, 55, 12),
-                  child: Text(
-                    "0",
-                    style: TextStyle(fontSize: 35, color: Colors.black),
-                  ),
-                ),
-              ),
-              numButton(".", Colors.greenAccent, Colors.black),
-              numButton("=", Colors.lightGreen, Colors.black),
+              numButton("0", Colors.green, Colors.black),
+              numButton("C", Colors.green, Colors.black),
+              numButton("=", Colors.green, Colors.black),
+              numButton("/", Colors.lightGreen, Colors.black),
             ]),
+            SizedBox(height: 10),
           ],
         ),
       ),
     );
+  }
+
+  //Calculator Logic
+  int firstNumber = 0;
+  int secondNumber = 0;
+  String result = "";
+  String text = "";
+  String operation = "";
+
+  void calculate(String btnText) {
+    if (btnText == "C") {
+      result = "";
+      text = "";
+      firstNumber = 0;
+      secondNumber = 0;
+    } else if (btnText == "/" ||
+        btnText == "X" ||
+        btnText == "-" ||
+        btnText == "+") {
+      firstNumber = int.parse(text);
+      result = "";
+      operation = btnText;
+    } else if (btnText == "=") {
+      secondNumber = int.parse(text);
+      if (operation == "+") {
+        result = (firstNumber + secondNumber).toString();
+      }
+      if (operation == "-") {
+        result = (firstNumber - secondNumber).toString();
+      }
+      if (operation == "X") {
+        result = (firstNumber * secondNumber).toString();
+      }
+      if (operation == "/") {
+        result = (firstNumber / secondNumber).toString();
+      }
+    } else {
+      result = int.parse(text + btnText).toString();
+    }
+
+    setState(() {
+      text = result;
+    });
   }
 }
